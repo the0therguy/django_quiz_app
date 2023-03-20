c5a49ee25bf8ef96d425c712888f7edfc79c4f3d
Mohammad BakiBillah Chowdhuryy (Ifty) <nblues399@gmail.com>
2023-03-21 01:10:19 +0600

    first table added

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class QuizCategory(models.Model):
+    title = models.CharField(max_length=200)
+    details = models.TextField()
+    image = models.ImageField(upload_to='quiz_category_image/')
+
+    def __str__(self):
+        return self.title
